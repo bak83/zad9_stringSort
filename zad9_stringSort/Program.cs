b8970d488ca97af6a45d72a3c217969d044a0fcf
bak83 <bak.dariusz@gmail.com>
2020-02-29 19:52:56 +0100

    x

--- zad9_stringSort/Program.cs
+++ zad9_stringSort/Program.cs
@@ -17,29 +17,33 @@ namespace zad9_stringSort
 			
 			string[] words = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
 			List<string> x = new List<string>();
+			Dictionary<string, int> W = new Dictionary<string, int>();
 
-			IEnumerable<string> sortDescendingQuery =
-		   from w in words
-		   orderby w descending
-		   select w;
-			foreach (var s in sortDescendingQuery)
+			foreach (var w in words)
 			{
-				Console.WriteLine(s);
+				string searchTerm = w;
+				var match = from word in words
+							where word.ToLowerInvariant() == searchTerm.ToLowerInvariant()
+							select word;
+				if (W.ContainsKey(w.ToString()))
+					continue;
+				else
+					W.Add(w.ToString(), match.Count());
 			}
-			//System.Console.WriteLine($"{words.Length} words in text:");
-			string searchTerm = "papugi";
-			var match = from word in words
-						where word.ToLowerInvariant() == searchTerm.ToLowerInvariant()
-						select word;
-			Console.WriteLine(match.Count());
-
+			//Console.WriteLine(match.Count());
+			
 
 			foreach (var word in words)
 			{
 				System.Console.WriteLine($"{word}");
 				//Console.WriteLine(word.Length);
-			;
+				;
 			}
+			foreach (var word in W)
+			{
+				Console.WriteLine("{0} - {1}",
+	   word.Key, word.Value);
+			}	
 
 			//Console.WriteLine("Ala ma kota i ma też psa, ale Ala nie ma papugi. Papugi ma Ola. ");
 			//string parse = "Ala ma kota i ma też psa, ale Ala nie ma papugi. Papugi ma Ola.";
@@ -47,6 +51,15 @@ namespace zad9_stringSort
 			//foreach (var x in parse)
 			//{
 			//	Console.WriteLine($"<{x}>");
+			//}
+
+			//IEnumerable<string> sortDescendingQuery =
+			//  from w in words
+			//  orderby w descending
+			//  select w;
+			//foreach (var s in sortDescendingQuery.Distinct())
+			//{
+			//	Console.WriteLine(s);
 			//}
 			Console.ReadKey();
 		}
